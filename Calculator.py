f1e8c936a52951ec78eb45cec1f89451cea3dc27
walusaka <walusakasamuel@gmail.com>
2025-07-28 21:48:47 +0300

    Calculator program

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+#Python calculator
+#Ask the user to enter the first number
+num1 = float(input("Enter first number: "))
+#Ask the user to enter the second number
+num2 = float(input("Enter second number: "))
+#Ask the user to enter the operator
+#Add the two numbers
+sum_result = num1 + num2
+#Subtract the second number from the first number
+difference_result = num1 - num2
+#Multiply the two numbers
+product_result = num1 * num2
+#Divide the first number by the second number
+quotient_result = num1 / num2
+#Display the results
+print(f"Sum: {sum_result}") #+
+print(f"Difference: {difference_result}")
+print(f"Product: {product_result}") #*
+print(f"Quotient: {quotient_result}") #/
